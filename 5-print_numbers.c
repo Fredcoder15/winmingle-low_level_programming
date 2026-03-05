7d80c2b5bf02eb1cf56d8c595dcb5543c26dc59e
Akpan-fred, Promise Friday <cerebralpromise2001@gmail.com>
2026-03-05 19:51:08 +0100

    Print all single-digit numbers of base 10 starting from 0.

---
+++ 5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Author: Akpan-fred, Promise Friday
+ *  Program: WinMingle Community C Training
+ *  Description: This file prints base 10 numbers from 0 to 9
+ */
+int main(void){
+     char c ='0';
+		 while(c<='9'){
+            putchar(c);
+			c++;
+		 }
+		 putchar('\n');
+		 return(0);
+}
+
