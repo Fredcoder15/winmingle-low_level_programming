bd5fb6223f97723291b3f8223f35ebb81b99eacd
Akpan-fred, Promise Friday <cerebralpromise2001@gmail.com>
2026-03-05 19:59:58 +0100

    Print all possible different combinations of three digits

---
+++ 101-print_comb4.c
@@ -0,0 +1,28 @@
+/*Author: Akpan-fred, Promise Friday
+ * Program: WinMingle Community C Training
+ *  Description: This prints combination of three  digit numbers.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int a, b, c;
+	for (a = 0; a <= 7; a++){
+		for (b = a + 1; b <= 8; b++){
+          for (c = b + 1; c <= 9; c++) {
+                 putchar('0' + a);
+				 putchar('0' + b);
+				 putchar('0' + c);
+	 if ( a != 7 || b != 8 || c != 9){
+        putchar (',');
+		putchar(' ');
+				 }
+	 else {
+		 putchar('.');
+	 }
+				  }
+		}
+	}
+	putchar('\n');
+	return(0);
+}
