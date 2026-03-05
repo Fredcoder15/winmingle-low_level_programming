151f19656211770446bd5e18620dfe2f4d456ad2
Akpan-fred, Promise Friday <cerebralpromise2001@gmail.com>
2026-03-05 19:53:16 +0100

    Print all single-digit numbers of base 10 starting from 0 without using char variables and only two putchar()

---
+++ 6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Author: Akpan-fred,Promise Friday
+ * Program: WinMingle Community C Training
+ *  Description: This program prints 
+ */
+
+int main(void){
+    int x= 0;
+    while(x<=10){
+     	putchar('0' + x);
+    	x++;
+        }
+     putchar('\n');
+     return(0);
+}
